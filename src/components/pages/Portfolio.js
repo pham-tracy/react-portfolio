b3225b9f3e7202013974cc471826226fd0252c4d
Tracy Pham <phamtracy95@gmail.com>
2023-01-09 22:19:05 -0800

    Add template for Portfolio page

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+
+      <div>
+        <h2>Project 1</h2>
+      </div>
+      <div>
+        <h2>Project 2</h2>
+      </div>
+      <div>
+        <h2>Project 3</h2>
+      </div>
+      <div>
+        <h2>Project 4</h2>
+      </div>
+      <div>
+        <h2>Project 5</h2>
+      </div>
+      <div>
+        <h2>Project 6</h2>
+      </div>
+    </div>
+  );
+}
