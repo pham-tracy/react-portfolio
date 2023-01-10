76604a90397ca259ada5a96575d6f30382a5e069
Tracy Pham <phamtracy95@gmail.com>
2023-01-09 22:16:59 -0800

    Add resume page content

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <h2>Education</h2>
+      <ul>
+        <li>UW Bootcamp</li>
+      </ul>
+      <h2>Skills</h2>
+      <h3>Front-End</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSSS</li>
+        <li>JavaScript</li>
+      </ul>
+      <h3>Back-End</h3>
+      <ul>
+        <li>Express</li>
+        <li>Node JS</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+}
