cde830427469cd3cd808ae3d0bb16adaa6449c39
Tracy Pham <phamtracy95@gmail.com>
2023-01-09 22:13:05 -0800

    Add contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <form className="form">
+        <label for="Name">Name</label>
+        <input name="Name" type="text" placeholder="Name" />
+
+        <br />
+
+        <label for="Email">Email</label>
+        <input name="Email" type="text" placeholder="Email" />
+
+        <br />
+        <label for="Message">Message</label>
+        <input name="Message" type="textarea" placeholder="Message" />
+
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
