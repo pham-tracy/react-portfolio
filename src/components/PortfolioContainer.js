223c0c0632f84825d2862a1276c950553c85d671
Tracy Pham <phamtracy95@gmail.com>
2023-01-11 22:57:45 -0800

    Add Portfolio Container code to render all pages

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import NavTabs from "./Navigation";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Portfolio from "./pages/Portfolio";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return (
+        <>
+          <Portfolio />
+        </>
+      );
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Header />
+
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <Footer />
+    </div>
+  );
+}
