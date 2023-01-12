713a1ce6771b6160792bc577c099c3465c818242
Tracy Pham <phamtracy95@gmail.com>
2023-01-11 22:35:29 -0800

    Add footer tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,22 +10,24 @@ const styles = {
 
 function Footer() {
   return (
-    <div style={styles.footer}>
-      <li>
-        <a href="https://github.com/pham-tracy">
-          {/* <img src="../public/github-mark.png" alt="GitHub logo">
+    <div>
+      <footer style={styles.footer}>
+        <li>
+          <a href="https://github.com/pham-tracy">
+            {/* <img src="../public/github-mark.png" alt="GitHub logo">
           {" "}
         </img> */}
-          GitHub
-        </a>
-      </li>
+            GitHub
+          </a>
+        </li>
 
-      <li>
-        <a href="https://www.linkedin.com/in/tracy-pham-377981140/">
-          {/* <img src="../public/LinkedInLogo.png" alt="LinkedIn logo"></img> */}
-          LinkedIn
-        </a>
-      </li>
+        <li>
+          <a href="https://www.linkedin.com/in/tracy-pham-377981140/">
+            {/* <img src="../public/LinkedInLogo.png" alt="LinkedIn logo"></img> */}
+            LinkedIn
+          </a>
+        </li>
+      </footer>
     </div>
   );
 }
