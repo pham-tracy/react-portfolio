b3e4b690f8f87062b43d891533621b08a3fe1edb
Tracy Pham <phamtracy95@gmail.com>
2023-01-11 22:27:10 -0800

    Update Footer to include Github & LinkedIn links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,22 +4,28 @@ const styles = {
   footer: {
     background: "white",
     fontSize: "20px",
-    color: "white",
+    color: "black",
   },
 };
 
 function Footer() {
   return (
     <div style={styles.footer}>
-      <a href="https://github.com/pham-tracy">
-        <img src="../public/github-mark.png" alt="GitHub logo">
+      <li>
+        <a href="https://github.com/pham-tracy">
+          {/* <img src="../public/github-mark.png" alt="GitHub logo">
           {" "}
-        </img>
-      </a>
+        </img> */}
+          GitHub
+        </a>
+      </li>
 
-      <a href="https://www.linkedin.com/in/tracy-pham-377981140/">
-        <img src="../public/LinkedInLogo.png" alt="LinkedIn logo"></img>
-      </a>
+      <li>
+        <a href="https://www.linkedin.com/in/tracy-pham-377981140/">
+          {/* <img src="../public/LinkedInLogo.png" alt="LinkedIn logo"></img> */}
+          LinkedIn
+        </a>
+      </li>
     </div>
   );
 }
