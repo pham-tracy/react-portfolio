2de6035c3a6972a43fac25ad88ba6da8a44cef0a
Tracy Pham <phamtracy95@gmail.com>
2023-01-14 23:11:37 -0800

    Remove header and footer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,7 +4,6 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Resume from "./pages/Resume";
 import Portfolio from "./pages/Portfolio";
-import Header from "./Header";
 import Footer from "./Footer";
 
 export default function PortfolioContainer() {
@@ -39,11 +38,9 @@ export default function PortfolioContainer() {
       {/* We are passing the currentPage from state and the function to update it */}
 
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      <Header />
 
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
-      <Footer />
     </div>
   );
 }
