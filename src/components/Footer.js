3d5bebb69cd50683184eff27e9343b53f5baae6a
Tracy Pham <phamtracy95@gmail.com>
2023-01-14 23:41:07 -0800

    Add Footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,22 +11,28 @@ const styles = {
 function Footer() {
   return (
     <div>
-      <footer style={styles.footer}>
-        <li>
-          <a href="https://github.com/pham-tracy">
-            {/* <img src="../public/github-mark.png" alt="GitHub logo">
-          {" "}
-        </img> */}
-            GitHub
-          </a>
-        </li>
+      <footer class="text-center">
+        <div class="container p-4">
+          <section class="mb-4">
+            {/* GitHub */}
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="https://github.com/pham-tracy"
+              role="button"
+            >
+              <i class="bi bi-github"></i>
+            </a>
 
-        <li>
-          <a href="https://www.linkedin.com/in/tracy-pham-377981140/">
-            {/* <img src="../public/LinkedInLogo.png" alt="LinkedIn logo"></img> */}
-            LinkedIn
-          </a>
-        </li>
+            {/* <!-- Linkedin --> */}
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="https://www.linkedin.com/in/tracy-pham-377981140/"
+              role="button"
+            >
+              <i class="bi bi-linkedin"></i>
+            </a>
+          </section>
+        </div>
       </footer>
     </div>
   );
