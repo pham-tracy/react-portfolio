d7207dd493c045de369fc54e6503dce4d8c3e477
Tracy Pham <phamtracy95@gmail.com>
2023-01-14 23:17:29 -0800

    Remove unncessary import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
