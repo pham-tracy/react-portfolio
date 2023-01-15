fb5fd725e74ac18efcf479bb5cb7352a9bfc6a7e
Tracy Pham <phamtracy95@gmail.com>
2023-01-14 23:28:03 -0800

    Update About me blurb

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,20 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>My name is Tracy Pham, and I am a Full-Stack Web Developer. </p>
+      <img src="" alt="profile-photo" />
+      <p>
+        My name is Tracy Pham, and I am a Full-Stack Web Developer based in
+        Seattle, Washington. I am currently pursing this certification through
+        the Unviersity of Washington's coding bootcamp and will graduate in
+        February 2023. I am passionate about utilizing technology to create a
+        lasting and impactful difference in our everyday lives.
+        <p>
+          {" "}
+          In my free time, I enjoy climbing, playing tennis, playing volleyball,
+          biking, hiking, and kayaking. When I'm not being sporty, I'm usually
+          inside reading a book (or two!).
+        </p>
+      </p>
     </div>
   );
 }
